f568326224ba8e779fde14acf701f01b9b770c57
Vinicius Maciel <vm.viniciusmaciel@gmail.com>
2025-09-30 18:34:43 -0300

    feat: enable local lua debugger

--- main.lua
+++ main.lua
@@ -1,6 +1,16 @@
 W, H = 800, 600
 DEBUG = true
 
+if os.getenv "LOCAL_LUA_DEBUGGER_VSCODE" == "1" then
+    local lldebugger = require "lldebugger"
+    lldebugger.start()
+    local run = love.run
+    function love.run(...)
+        local f = lldebugger.call(run, false, ...)
+        return function(...) return lldebugger.call(f, false, ...) end
+    end
+end
+
 function love.load()
     love.graphics.setDefaultFilter('nearest', 'nearest')
     love.audio.setVolume(0.5)
